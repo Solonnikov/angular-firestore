b29f313d3a4f9a3c2449b6a9409ab47744b49a2f
Yaroslav Solonnikov <yaroslav.solonnikov@gmail.com>
2017-10-29 00:44:08 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environments/environment.prod';
+
+import { ItemService } from './services/item.service';
+
+import { AppComponent } from './app.component';
+import { ItemsComponent } from './components/items/items.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(environment.firebase, 'angularfs'),
+    AngularFirestoreModule
+  ],
+  providers: [
+    ItemService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAS2dRwTnKeKv_0aDdxMV-7-YHvKnn0PBA",
+    authDomain: "fs1prod-12815.firebaseapp.com",
+    databaseURL: "https://fs1prod-12815.firebaseio.com",
+    projectId: "fs1prod-12815",
+    storageBucket: "fs1prod-12815.appspot.com",
+    messagingSenderId: "943935939521"
+  }
+};
